01b1f49b8579fa1d217c47ea0ee105db731a8fad
TejeshCodes <tejeshreddy907@gmail.com>
2021-05-10 23:41:09 +0530

    prog 12 is made neat

--- src/Program12.java
+++ src/Program12.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
 public class Program12 {
+    public static Scanner sc = new Scanner(System.in);
+    public static String str;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        accept();
+        printResult();
+    }
+
+
+    public static void accept() {
+        System.out.print("Enter a word: ");
+        str = sc.next();
         sc.close();
-
-        int pos = checkPos(str);
-
-        if (pos > -1) {
-            str = str.substring(pos) + str.charAt(0) + str.substring(0, pos - 1) + "ay";
-        }
-        System.out.println("The word in Pig Latin is: " + str);
+        System.out.println();
     }
 
     public static int checkPos(String str) {
@@ -24,5 +27,13 @@ public class Program12 {
             }
         }
         return -1;
+    }
+
+    public static void printResult() {
+        int pos = checkPos(str);
+        if (pos > -1) {
+            str = str.substring(pos) + str.charAt(0) + str.substring(0, pos - 1) + "ay";
+        }
+        System.out.println("The word in Pig Latin is: " + str);
     }
 }
