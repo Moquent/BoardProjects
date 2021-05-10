487b26dfad45c3706c0f808261e4bfe8f1b06c2c
TejeshCodes <tejeshreddy907@gmail.com>
2021-05-10 22:35:40 +0530

    changed "number" to "word" while printing

--- src/Program14.java
+++ src/Program14.java
@@ -6,11 +6,11 @@ public class Program14 {
         String str = sc.next();
         sc.close();
         if (str.charAt(0) != str.charAt(str.length() - 1))
-            System.out.println("This is not a special number.");
+            System.out.println("This is not a special word.");
         else {
             for (int i = 0; i < str.length(); i++) {
                 if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
-                    System.out.println("This is a special number, but not a palindrome.");
+                    System.out.println("This is a special word, but not a palindrome.");
                     break;
                 }
             }
