def2457456d1feae7869722b5711de3dbbcc3b80
TejeshCodes <tejeshreddy907@gmail.com>
2021-05-10 22:33:02 +0530

    program 14 done.

---
+++ src/Program14.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Program14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        if (str.charAt(0) != str.charAt(str.length() - 1))
+            System.out.println("This is not a special number.");
+        else {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                    System.out.println("This is a special number, but not a palindrome.");
+                    break;
+                }
+            }
+            System.out.println("This is a palindrome.");
+        }
+    }
+}
