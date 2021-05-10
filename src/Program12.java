ddde099ed3f479bbcf8e3dd2cb8f4ac290fddc6e
TejeshCodes <tejeshreddy907@gmail.com>
2021-05-10 22:23:19 +0530

    12 and 13 done.

--- src/Program12.java
+++ src/Program12.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Program12 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+
+        int pos = checkPos(str);
+
+        if (pos > -1) {
+            str = str.substring(pos) + str.charAt(0) + str.substring(0, pos - 1) + "ay";
+        }
+        System.out.println("The word in Pig Latin is: " + str);
+    }
+
+    public static int checkPos(String str) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < str.length(); i++) {
+            for (char vowel : vowels) {
+                if (str.charAt(i) == vowel) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
---
+++ src/Program13.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Program13 {
+    public static int[][] m = {{12, 23, 33, 22}, {20, 31, 44, 10}, {10, 25, 14, 8}, {41, 52, 24, 12}};
+    public static int[][] n = {{8, 22, 31, 15}, {11, 16, 30, 26}, {10, 17, 25, 33}, {12, 24, 21, 32}};
+    public static int[][] p = new int[4][4];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                p[i][j] = m[i][j] + n[i][j];
+                System.out.print(p[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.deepToString(p));
+    }
+}
