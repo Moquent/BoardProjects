1e29f3ee15592dc50090b45cc7babd00191ac71b
TejeshCodes <tejeshreddy907@gmail.com>
2021-05-10 23:02:59 +0530

    program 2 cleaned.

--- src/Program2A.java
+++ src/Program2A.java
@@ -3,23 +3,29 @@ import java.util.Scanner;
 public class Program2A {
     public static Scanner sc = new Scanner(System.in);
     public static String num;
+    public static int total = 0;
 
     public static void main(String[] args) {
-
-
-        int total = 0;
-        for (int i = 0; i < num.length(); i++)
-            total += Integer.parseInt(Character.toString(num.charAt(i)));
-
-        if (Integer.parseInt(num) % total == 0)
-            System.out.println("This is a Harshad Number.");
-        else
-            System.out.println("This is not a Harshad Number.");
+        accept();
+        calculateTotal();
+        printResult();
     }
 
     public static void accept() {
         System.out.println("Enter a number: ");
         num = sc.next();
         sc.close();
+    }
+
+    public static void calculateTotal() {
+        for (int i = 0; i < num.length(); i++)
+            total += Integer.parseInt(Character.toString(num.charAt(i)));
+    }
+
+    public static void printResult() {
+        if (Integer.parseInt(num) % total == 0)
+            System.out.println("This is a Harshad Number.");
+        else
+            System.out.println("This is not a Harshad Number.");
     }
 }
