f6ef075581ca9a40524d6e73f197f0db93916297
TejeshCodes <tejeshreddy907@gmail.com>
2021-05-10 22:47:17 +0530

    program 15 done.

---
+++ src/Program15.java
@@ -0,0 +1,34 @@
+class BookFair {
+    public static String BName;
+    public static double price;
+    public static double discountedPrice;
+
+    public BookFair() {
+        BName = "";
+        price = 0;
+        discountedPrice = 0;
+    }
+
+    public static void calculate() {
+        if (price <= 1000) discountedPrice = (98.0 / 100.0) * price;
+        else if (price <= 3000) discountedPrice = (90.0 / 100.0) * price;
+        else if (price > 3000) discountedPrice = (85.0 / 100.0) * price;
+    }
+
+    public static void display() {
+        System.out.println("Name of the book: " + BName);
+        System.out.println("MRP of the book: " + price);
+        System.out.println("Discounted price of the book: " + discountedPrice);
+    }
+}
+
+public class Program15 {
+    public static void main(String[] args) {
+        BookFair book = new BookFair();
+        book.BName = "Harry Potter";
+        book.price = 499;
+
+        book.calculate();
+        book.display();
+    }
+}
