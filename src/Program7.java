56dafb97763eacb474b5bd758430cf34952de953
Moquent <tejeshreddy907@gmail.com>
2021-05-16 14:47:32 +0530

    ok 7th one done

--- src/Program7.java
+++ src/Program7.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Program7 {
-    public static Scanner sc = new Scanner(System.in);
-    public static int[][] arr = new int[3][3];
-
-    public static void main(String[] args) {
-        accept();
-        System.out.println(Arrays.deepToString(arr));
-        System.out.println(calculate(arr));
-
-    }
-
-    public static void accept() {
-        for (int i = 0; i < 3; i++) {
-            String[] nums = sc.nextLine().split(" ");
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(nums[j]);
-            }
-        }
-    }
-
-    public static int calculate(int[][] arr) {
-        int sum = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                sum += arr[i][j];
-            }
-        }
-        return sum;
-    }
-}
-=======
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -76,4 +41,3 @@ public class Program7 {
         return sum;
     }
 }
->>>>>>> done with the project
