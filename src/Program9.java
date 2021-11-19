e85ab8d6c03d841aef5602cae319d70bf50a324f
Moquent <tejeshreddy907@gmail.com>
2021-11-19 11:22:18 +0530

    Update Program9.java

--- src/Program9.java
+++ src/Program9.java
@@ -1,10 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Program9 {
 
     // make a main function to run the program
     public static void main(String[] args) {
 
-        // run the function to check
-        check("HELLO THERE", 'E');
+        // type 1
+        check("HELLO THERE", "E");
+
+        // type 2
+        check("HELLO THERE");
     }
 
     // type 1 (overloading)
@@ -12,11 +19,9 @@ public class Program9 {
 
         // check if a character is present in the string "str"
         int n = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) {
+        for (int i = 0; i < str.length(); i++)
+            if (str.charAt(i) == ch)
                 n++;
-            }
-        }
 
         // print result
         System.out.println("Number of " + ch + " present is " + n);
@@ -25,35 +30,11 @@ public class Program9 {
     // type 2 (overloading)
     public static void check(String s1) {
 
-        // check if a vowel is present in the string "s1"
+        // print all the vowels in the string "si"
         s1 = s1.toLowerCase();
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        char[] letters = new char[5];
-        int k = 0;
-
         for (int i = 0; i < s1.length(); i++) {
-            for (char vowel : vowels) {
-                if (s1.charAt(i) == vowel) {
-                    boolean isInArr = false;
-
-                    for (char letter : letters) {
-                        if (letter == vowel) {
-                            isInArr = true;
-                            break;
-                        }
-                    }
-
-                    if (!isInArr) {
-                        letters[k] = vowel;
-                        k++;
-                    }
-                }
-            }
-        }
-
-        // print all vowels present in the string "s1"
-        for (char letter : letters) {
-            System.out.print(letter + " ");
+            if (s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i' || s1.charAt(i) == 'o' || s1.charAt(i) == 'u')
+                System.out.println(s1.charAt(i));
         }
     }
 }
